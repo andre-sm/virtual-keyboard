7b8cbe34d1254335bf8279eb7d48c4fd1cfd302d
Andrei Smolich <andy.smolich@gmail.com>
2022-05-07 17:32:21 +0200

    feat: implement shift button logic

--- src/js/index.js
+++ src/js/index.js
@@ -28,9 +28,56 @@ const createKeys = () => {
     } else {
       keyBtn.textContent = keyItem.text;
     }
+    if (keyItem.type === 'capsLock') {
+      const light = document.createElement('span');
+      light.classList.add('capslock-status');
+      light.innerText = '●';
+      keyBtn.classList.add('capslock');
+      keyBtn.append(light);
+    }
     fragment.append(keyBtn);
   });
   return fragment;
+};
+
+const toUpperCase = (letters) => {
+  const lang = settings.language;
+  letters.forEach((item) => {
+    const { code } = item.dataset;
+    const letter = item;
+    if (settings.caps) {
+      letter.textContent = keysObj[code][lang].shiftOn.toLowerCase();
+    } else {
+      letter.textContent = keysObj[code][lang].shiftOn;
+    }
+  });
+};
+
+const toLowerCase = (letters) => {
+  const lang = settings.language;
+  letters.forEach((item) => {
+    const { code } = item.dataset;
+    const letter = item;
+    if (settings.caps) {
+      letter.textContent = keysObj[code][lang].shiftOff.toUpperCase();
+    } else {
+      letter.textContent = keysObj[code][lang].shiftOff;
+    }
+  });
+};
+
+const onActiveShift = (shifts, letters) => {
+  shifts.forEach((shift) => {
+    shift.addEventListener('mousedown', () => {
+      shift.classList.add('pressed');
+      toUpperCase(letters);
+    });
+
+    shift.addEventListener('mouseup', () => {
+      shift.classList.remove('pressed');
+      toLowerCase(letters);
+    });
+  });
 };
 
 const createKeyboard = () => {
@@ -53,6 +100,10 @@ const createKeyboard = () => {
 
   const keys = createKeys();
   keyboard.append(keys);
+
+  const shiftBtns = document.querySelectorAll('[data-type="shift"]');
+  const letters = document.querySelectorAll('[data-type="standart"]');
+  onActiveShift(shiftBtns, letters);
 };
 
 window.onload = () => {
